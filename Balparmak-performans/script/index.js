a0800a92f3153cb01eb13afb8ab4029481273d55
Ömer Çetiner <omer39ctnr@gmail.com>
2023-08-18 10:56:30 +0300

    resim değişimi eklendi
    🤔

---
+++ Balparmak-performans/script/index.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("res1").addEventListener("click",function(){
+    document.getElementById("resim").style.backgroundImage="url(image/res1.jpg)";
+});
+document.getElementById("res2").addEventListener("click",function(){
+    document.getElementById("resim").style.backgroundImage="url(image/res2.jpg)";
+});
+document.getElementById("res3").addEventListener("click",function(){
+    document.getElementById("resim").style.backgroundImage="url(image/res3.jpg)";
+});

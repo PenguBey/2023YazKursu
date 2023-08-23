d8e57f75508004a1330fa0b4e92926e9d46d537b
Ömer Çetiner <omer39ctnr@gmail.com>
2023-08-23 10:12:28 +0300

    topların renk değişimi yapıldı

--- balparmak-birleştirme/script/index.js
+++ balparmak-birleştirme/script/index.js
@@ -1,9 +1,23 @@
+function renk() {
+    for (let i = 1; i < 4; i++) {
+        document.getElementById("res"+i).style.backgroundColor = "#c9c9c9";
+
+    }
+    document.getElementById(sari).style.backgroundColor = "yellow";
+}
+var sari = ""
 document.getElementById("res1").addEventListener("click",function(){
+    sari = "res1";
     document.getElementById("resim").style.backgroundImage="url(image/res1.jpg)";
+    renk();
 });
 document.getElementById("res2").addEventListener("click",function(){
+    sari = "res2";
     document.getElementById("resim").style.backgroundImage="url(image/res2.jpg)";
+    renk();
 });
 document.getElementById("res3").addEventListener("click",function(){
+    sari = "res3";
     document.getElementById("resim").style.backgroundImage="url(image/res3.jpg)";
+    renk();
 });
